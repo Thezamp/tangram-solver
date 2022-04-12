d3daeac005afcdefebc8b7f2167756a2484af07b
Thezamp <giacomo.zamprogno97@gmail.com>
2022-04-12 14:26:47 +0200

    house continued (first triggers/removes)

--- house_with_chimney.py
+++ house_with_chimney.py
@@ -28,7 +28,22 @@ def generate_house_with_chimney_landmarks():
     small_t_chimney_high = Landmark('SMALL-T-CHIMNEY-HIGH','SMALL-T','CHIMNEY','WEAK') #PROBABLY WORKS
     small_t_chimney_low = Landmark('SMALL-T-CHIMNEY-LOW','SMALL-T','CHIMNEY','WEAK')
 
+    #####triggers and removes
+    chimney.add_removes([small_t_chimney_low,small_t_chimney_high])
+    chimney.add_triggers([roof_left])
+    roof_right.add_removes([big_t_low_right_corner,big_t_roof_left])
+    roof_right.add_triggers([roof_left])
+    roof_left.add_removes([big_t_roof_left,big_t_low_left_corner])
 
+    ####
+    wall_up.add_removes([wall_down, big_t_low_left_corner,big_t_low_right_corner])
+    wall_up.add_triggers([mid_t_right_point,mid_t_left_point,small_t_right_point,small_t_left_point,small_t_right_comb_up,
+                          small_t_right_comb_down,small_t_left_comb_down,small_t_left_comb_up])
+    mid_t_left_point.add_removes([big_t_low_left_corner,big_t_roof_left,
+                                  mid_t_right_point,small_t_left_point,small_t_left_comb_up,small_t_left_comb_down])
+    mid_t_right_point.add_removes([big_t_low_right_corner,
+                                   mid_t_left_point,small_t_right_point,small_t_right_comb_up,small_t_right_comb_down])
+    #small_t_left_point.add_removes([small_t_right_comb_up,small_t_left_comb_down,mid_t_right_point])
     pass
 
 
