d613b549cdfe80c4b2d389da38356b7a1d61cd9c
Thezamp <giacomo.zamprogno97@gmail.com>
2022-05-11 11:28:20 +0200

    more docstrings

--- landmark_detector/landmark_extraction.py
+++ landmark_detector/landmark_extraction.py
@@ -14,11 +14,16 @@ solution_limits = {1: [(-260, 120), (-120, 140)], 2: [(-280, -20), (-80, 200)], 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def get_grid_value(rm, cm, tgn):
     '''
     Converts x and y matrix coordinates into grid values for tangram puzzle = tgn
 
-    outputs the grid number 0-15, or -1 in case the placement is generically outside the grid
+    :param rm: row value in matrix coordinates
+    :type rm: int
+    :param cm: column value in matrix coordinates
+    :type cm: int
+    :return:  grid number 0-15, or -1 if outside the grid
     '''
 
     y = -rm + 300
@@ -40,12 +45,15 @@ def get_grid_value(rm, cm, tgn):
 def generate_rotation_templates(original, rotations):
     '''generates #rotations rotated and cropped templates from an original tan picture
 
-    :param nparray original: the original uncropped template picture
-    :param int rotations: number of rotations to be produced (varies due to symmetries)
+    :param original: the original uncropped template picture
+    :type original: numpy.array
+    :param rotations: number of rotations to be produced (varies due to symmetries)
+    :type rotations: int
 
     :return: the list of rotated and cropped templates
+    :rtype: list[numpy.array]
     '''
-    original = original[3:,3:]
+    original = original[3:, 3:]
     nonzero = original.nonzero()
     templates = [original[min(nonzero[0]) - 2:max(nonzero[0]) + 2, min(nonzero[1]) - 2:max(nonzero[1]) + 2]]
     for r in range(-45, -45 * rotations, -45):
@@ -61,11 +69,15 @@ def find_placements(piece, state_img, edged_image):
     """
     Applies template matching in order to find a number of possible placements for the given piece
 
-    :param Template piece: an object of class Template representing the piece
-    :param ndarray state_img: the binarized image of the puzzle state
-    :param ndarray edged_image: the image after the application of Canny
+    :param piece: an object of class Template representing the piece
+    :type piece: Template
+    :param state_img: the binarized image of the puzzle state
+    :type state_img: ndarray
+    :param edged_image: the image after the application of Canny
+    :type edged_image: ndarray
 
-    :returns List(Tuples): a list of possible (imprecise) placements for the given piece
+    :return: a list of possible (imprecise) placements for the given piece, in matrix coordinate
+    :rtype: list[(int,int)]
     """
     available_placements = []
     for ti in range(len(piece.templates)):
@@ -74,9 +86,8 @@ def find_placements(piece, state_img, edged_image):
         h, w = current.shape[:2]
         method = cv.TM_SQDIFF
 
-        #res = cv.matchTemplate(edged_image, edges, method, mask=current)
+        # res = cv.matchTemplate(edged_image, edges, method, mask=current)
         res = cv.matchTemplate(edged_image, edges, method)
-
 
         i = 0
         attempt = 0
@@ -91,8 +102,8 @@ def find_placements(piece, state_img, edged_image):
             res[xt:xb, yl:yr] = float('inf')
             central_coord = (min_loc[1] + h // 2, min_loc[0] + w // 2)  # center of the bounding box
             # if (state_img[central_coord] == 1):
-            part = state_img[min_loc[1]:min_loc[1]+h,min_loc[0]:min_loc[0]+w]
-            if np.count_nonzero(np.bitwise_xor(np.bitwise_and(part,current),current)) <50 :
+            part = state_img[min_loc[1]:min_loc[1] + h, min_loc[0]:min_loc[0] + w]
+            if np.count_nonzero(np.bitwise_xor(np.bitwise_and(part, current), current)) < 50:
                 # print((min_loc[1]+h//2,min_loc[0]+w//2))
                 if '-T' in piece.name:  # coordinates in the app are calculated on the long side
                     if ti == 0:
@@ -113,13 +124,14 @@ def find_placements(piece, state_img, edged_image):
 
 
 class Template:
-    def __init__(self, name, path, rotations, flipped= False):
+    def __init__(self, name, path, rotations, flipped=False):
         self.name = name
         original_rotation = cv.imread(path, 0)
         t2, original_rotation = cv.threshold(original_rotation, 100, 1, cv.THRESH_BINARY)
 
         self.templates = generate_rotation_templates(original_rotation, rotations)
         self.flipped = flipped
+
     def describe(self):
         if not self.flipped:
             return self.name
@@ -130,12 +142,12 @@ class Template:
 class LandmarkExtractor:
     def __init__(self, counts, tgn=4):
         self.pieces_templates = [Template('SMALL-T', f'{ROOT_DIR}/tans/smallt.png', 7),
-                       Template('BIG-T',  f'{ROOT_DIR}/tans/bigt.png', 7),
-                       Template('MIDDLE-T',  f'{ROOT_DIR}/tans/middlet.png', 7),
-                       Template('SQUARE',  f'{ROOT_DIR}/tans/square.png', 2),
-                       Template('PARALL',  f'{ROOT_DIR}/tans/parall1.png', 3),
-                       Template('PARALL',  f'{ROOT_DIR}/tans/parall2.png', 3, flipped=True)
-                       ]
+                                 Template('BIG-T', f'{ROOT_DIR}/tans/bigt.png', 7),
+                                 Template('MIDDLE-T', f'{ROOT_DIR}/tans/middlet.png', 7),
+                                 Template('SQUARE', f'{ROOT_DIR}/tans/square.png', 2),
+                                 Template('PARALL', f'{ROOT_DIR}/tans/parall1.png', 3),
+                                 Template('PARALL', f'{ROOT_DIR}/tans/parall2.png', 3, flipped=True)
+                                 ]
         self.tgn = tgn
 
         self.counts = counts
@@ -148,12 +160,12 @@ class LandmarkExtractor:
         state = cv.imread(image_path, 0)
 
         state[500:, :] = 0
-        state= state[:,:450]
+        state = state[:, :450]
         _, state = cv.threshold(state, 240, 1, cv.THRESH_BINARY)
-        cv.imwrite('./utility_pictures/thresholded.png',state*255)
+        cv.imwrite('./utility_pictures/thresholded.png', state * 255)
         # edges
         edged_image = cv.Canny(state * 255, 100, 200)
-        cv.imwrite('./utility_pictures/edged.png',edged_image)
+        cv.imwrite('./utility_pictures/edged.png', edged_image)
         extracted_landmarks = set()
         for piece in [x for x in self.pieces_templates if x.name in pieces_list]:
 
@@ -166,17 +178,16 @@ class LandmarkExtractor:
                 grid = get_grid_value(p[0][0], p[0][1], self.tgn)
                 if grid != -1:
 
-
                     row = piece_counts.loc[(piece_counts['grid_val'] == grid) & \
-                                     (piece_counts['rot'] == rot)]
+                                           (piece_counts['rot'] == rot)]
                     if not row.empty:
                         ldm_count = row['counts'].values
                         piece_landmarks.add((piece.describe(), grid, rot, ldm_count[0]))
 
-
             extracted_landmarks.update(piece_landmarks)
 
-        if set([x[0] if "PARALL" not in x[0] else "PARALL" for x in extracted_landmarks]) != set(pieces_list) and set(pieces_list).issubset(set(counts['item'].tolist())):
+        # if set(pieces from extracted landmarks) != set(available pieces) and human data exists for all the available pieces at current time
+        if set([x[0] if "PARALL" not in x[0] else "PARALL" for x in extracted_landmarks]) != set(pieces_list) and set(
+                pieces_list).issubset(set(counts['item'].tolist())):
             problem = True
         return sorted(list(extracted_landmarks), reverse=True, key=lambda x: x[3]), problem
-
