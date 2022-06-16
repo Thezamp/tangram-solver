b568cc3d420b764cd965fca49fe33338e5882268
Thezamp <giacomo.zamprogno97@gmail.com>
2022-06-16 16:34:04 +0200

    fixed process_logs.ipynb

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -386,10 +386,10 @@ if __name__ == '__main__':
                            'big triangle':s[i][2],'square':s[i][3],'parallelogram':s[i][4]}
             results_df = results_df.append(row,ignore_index=True)
 
-    results_df.to_csv('datasets/model_states_evolution_4_2.csv')
+    results_df.to_csv('datasets/model_states_evolution_4_twolevels.csv')
     length = max(map(len, to_mat))
     mat = np.array([xi + [0] * (length - len(xi)) for xi in to_mat])
-    np.savetxt("datasets/heatmap_4_2.csv", mat, delimiter=',')
+    np.savetxt("datasets/heatmap_4_twolevels.csv", mat, delimiter=',')
 
 
 '''
