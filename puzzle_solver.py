92c032161df9ee08c8571623d90a8475784b5e4b
Thezamp <giacomo.zamprogno97@gmail.com>
2022-07-13 12:28:48 +0200

    merged

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -6,10 +6,11 @@ import pandas as pd
 
 import actr
 from landmark import Landmark
-from landmark_detector.landmark_extraction import LandmarkExtractor, get_grid_value
+from landmark_detector.landmark_extraction import LandmarkExtractor, get_grid_value, solution_limits
 from application.create_state import setpos
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 #big t, big t, middle t, small t, small t, square, parall
 puzzle_def = {1: {'pos': [(250, 99, 180), (349, 0, 270), (200, -49, 225), (151, 49, 90), (250, -49, 0), (200, 0, 45),
@@ -71,14 +72,16 @@ def puzzle_state_to_imaginal(ldm_list, problem, available):
         return []
 
     current_landmarks = []
-
+    ldm_i = 0
     for i in range(len(ldm_list)):
         l = Landmark(ldm_list[i])
-        current_landmarks.append(l)
-        if i < 6:
-            state_def.append(f'LANDMARK-{i + 1}')
+
+        if len(current_landmarks) < 6 and l.name not in [x.name for x in current_landmarks]:
+            current_landmarks.append(l)
+            state_def.append(f'LANDMARK-{ldm_i + 1}')
 
             state_def.append(l.name)
+            ldm_i +=1
 
     if problem:
         state_def.append('SPECIAL-LANDMARK')
@@ -110,7 +113,7 @@ class Piece():
 
 class Puzzle():
 
-    def __init__(self, tgn, params_dict, path="ACT-R:tangram-solver;models;solver-model.lisp"):
+    def __init__(self, tgn, params_dict, path="ACT-R:tangram-solver;models;solver-model.lisp", predictor = False):
 
         #self.completed = False
         self.status = None
@@ -131,12 +134,15 @@ class Puzzle():
         self.counts = []
         self.step = 0
         self.btsteps = 0
+        self.predictor = predictor
+        self.prediction_answer = []
 
         data = pd.read_csv(f'{ROOT_DIR}/datasets/steps.csv')
         self.players_data = data.loc[data['tangram nr'] == tgn]
 
-        for phase in [4, 8, 12, 16]:
-            phase_counts = pd.read_csv(f'{ROOT_DIR}/datasets/landmark_str_{phase + 1}.csv')
+        #for phase in [5, 9, 13, 17]:
+        for phase in range(4):
+            phase_counts = pd.read_csv(f'{ROOT_DIR}/datasets/landmark_str_{phase}.csv')
             self.counts.append(phase_counts.loc[phase_counts['tangram nr'] == tgn])
 
         self.extractor = LandmarkExtractor(self.counts, tgn)
@@ -173,14 +179,23 @@ class Puzzle():
         The tuple (Piece, Landmark) is added to the step sequence and to the current placements. The position in the
         experiment window is taken from the human data
         """
+        if self.predictor:
+            print(f'Suggest update {piece_type}, {rotation} at {grid} ')
+            if piece_type != 'PARALL-INV':
+                self.prediction_answer.append(('Place', (piece_type,int(grid),int(rotation))))
+            else:
+                self.prediction_answer.append(('Place', ('PARALL', int(grid), int(rotation))))
+            return True
+
+        if piece_type=='BIG-T' and grid ==17.0:
+            print('halt')
         self.status = 'updating'
         try:
             chosen_landmark = [x for x in self.current_landmarks if x.is_involved(piece_type, grid, rotation)][
                 0]  # landmark that has been selected
             print('landmark')
         except IndexError:
-            print('retrieved wrong landmark')
-            return True
+            return False
         if piece_type == 'PARALL':
             self.pos[7] = 1
         if piece_type == 'PARALL-INV':
@@ -197,12 +212,19 @@ class Puzzle():
         x, y = pixel_rows.sort_values(by='counts', ascending=False)[['x', 'y']].iloc[0]
 
         # named_piece = next(x for x in self.available_pieces if x.type == piece_type)
-        named_piece = [x for x in self.available_pieces if x.type == piece_type][0]
-        print('piece')
-        self.available_pieces.remove(named_piece)
-        print('removed')
-        self.pos[named_piece.index] = (x, y, rotation)
-        print(f'pos of {named_piece.index}')
+        try:
+            named_piece = [x for x in self.available_pieces if x.type == piece_type][0]
+        except IndexError:
+            return False
+        try:
+            self.available_pieces.remove(named_piece)
+        except IndexError:
+            return False
+        try:
+            self.pos[named_piece.index] = (x, y, rotation)
+        except IndexError:
+            return False
+        # print(f'pos of {named_piece.index}')
         print(f'action taken: {named_piece.name}-{rotation} at grid pos {grid}')
 
         self.step_sequence.append((named_piece.name, grid, rotation))
@@ -213,6 +235,10 @@ class Puzzle():
         return True
 
     def piece_backtrack(self):
+        if self.predictor:
+            print('Suggest backtracking weak piece')
+            self.prediction_answer.append(('Backtrack',None))
+            return True
         self.btsteps += 1
         self.status = "piece_backtracking"
         # frequencies = [l.get_frequency(self.counts[self.step // 4]) for (l, p) in self.current_placements]
@@ -232,20 +258,25 @@ class Puzzle():
         return True
 
     def region_backtrack(self):
+        if self.predictor:
+            print('Suggest backtracking problem piece')
+            self.prediction_answer.append(('Backtrack', None))
+            return  True
         self.status = "region_backtracking"
         self.btsteps += 1
-        landmark, named_piece = self.problem_placements.pop(0)
+        if len(self.problem_placements) != 0:
+            landmark, named_piece = self.problem_placements.pop(0)
 
-        self.pos[named_piece.index] = puzzle_def.get(self.tgn).get('pos')[named_piece.index]
-        self.available_pieces.append(named_piece)
+            self.pos[named_piece.index] = puzzle_def.get(self.tgn).get('pos')[named_piece.index]
+            self.available_pieces.append(named_piece)
 
-        print(f'backtracking piece: {named_piece.name}')
+            print(f'backtracking piece: {named_piece.name}')
 
-        self.step_sequence.append((named_piece.name, -1, landmark.rotation))
-        self.current_placements.remove((landmark, named_piece))
-
-
-        return True
+            self.step_sequence.append((named_piece.name, -1, landmark.rotation))
+            self.current_placements.remove((landmark, named_piece))
+            return True
+        else:
+            return self.piece_backtrack()
 
     def flag_completed(self):
         self.status = "completed"
@@ -271,8 +302,9 @@ class Puzzle():
         return state
 
     def grid_from_turtle(self,x,y):
-        solution_limits = {1: [(-260, 120), (-120, 140)], 2: [(-280, -20), (-80, 200)], 3: [(-320, 60), (-140, 140)],
-                           4: [(-280, 0), (-200, 300)]}
+        # solution_limits = {1: [(-260, 120), (-120, 140)], 2: [(-280, 0), (-70, 210)], 3: [(-320, 60), (-140, 140)],
+        #                    4: [(-280, 0), (-200, 300)]}
+        global solution_limits
         xrange = solution_limits.get(self.tgn)[0]
         yrange = solution_limits.get(self.tgn)[1]
         xstep = (xrange[1] - xrange[0]) / 5
@@ -285,9 +317,91 @@ class Puzzle():
 
         return ygrid * 5 + xgrid
 
-def onerun(params_dict):
+def prediction_run(pzn,params_dict,kd=1,kcv=2):
+    steps =  pd.read_csv('./datasets/all_steps_test_ZAL.csv')
+    steps = steps.loc[steps['tangram nr'] == pzn]
+
+
+    acc =[]
+
+
+    for participant in steps.sid.unique():
+        score = 0
+        max_score = 0
+        p = Puzzle(pzn, params_dict, path="ACT-R:tangram-solver;models;predictor-model.lisp", predictor=True)
+        p.path = f'{ROOT_DIR}/puzzle_state.png'
+        if pzn == 4:
+            p.pos[-1] = -1
+        setpos(p.pos, p.sol, True)
+
+        participant_steps = steps.loc[steps.sid == participant]
+        pieces = [['BIG-T',True],['BIG-T',True],['MIDDLE-T',True],['SMALL-T',True],['SMALL-T',True],['SQUARE',True],['PARALL',True]]
+        p.step = 0
+
+        for i_step in range(len(participant_steps)-2):
+            p.prediction_answer=[]
+            row = participant_steps.iloc[i_step]
+            if p.step >16:
+                break
+
+
+            partial = participant_steps[:i_step+1]
+
+
+            print(f'participant action: {row["item"]}, {row.rot} at {row.grid_val} ')
+            print('###')
+            p.pos[row['i_item']] = (row.x,row.y,row.rot)
+            setpos(p.pos, p.sol)
+            for pi in participant_steps.i_item.unique():
+                pos = partial.loc[partial.i_item == pi]
+                if pos.empty:
+                    pieces[pi][1] = True
+                elif pos.iloc[-1]['step'] == 100:
+                    pieces[pi][1] = True
+                else:
+                    pieces[pi][1] = False
+
+            available_pieces = [p[0] for p in pieces if p[1]]
+
+            #Agent Part - prediction
+            if row['step'] != 100.0:
+                max_score += 1
+                p.step = int(row.step)+1
+                print(p.step)
+                (extract, problem) = p.extractor.extract(p.path, available_pieces, p.step,kd=kd,kcv=kcv)
+
+
+
+
+                for i in range(10):
+                    p.current_landmarks = puzzle_state_to_imaginal(extract, problem, len(available_pieces))
+                    actr.goal_focus('start')
+                    p.run()
+
+                answer = max(set(p.prediction_answer), key=p.prediction_answer.count) if len(p.prediction_answer) else ['Skip',None]
+                if answer[0] == 'Place':
+                    p_move = answer[1]
+                    next_row = participant_steps.iloc[i_step+1]
+                    second_next_row = participant_steps.iloc[i_step +2]
+                    print(f'next: {(next_row["item"],int(next_row["grid_val"]),int(next_row["rot"]))} ')
+                    print(f'second next: {(second_next_row["item"], int(second_next_row["grid_val"]), int(second_next_row["rot"]))}  ')
+                    if (p_move == (next_row['item'],int(next_row['grid_val']),int(next_row['rot'])) or
+                            p_move == (second_next_row['item'], int(second_next_row['grid_val']), int(second_next_row['rot'])) ):
+                        score +=1
+                elif answer[0] == 'Backtrack':
+                    print(participant_steps.iloc[i_step+1]['step'])
+                    print(participant_steps.iloc[i_step+2]['step'])
+                    if (participant_steps.iloc[i_step+1]['step'] == 100
+                            or participant_steps.iloc[i_step+2]['step'] == 100):
+                        score +=1
+        acc.append(score/max_score)
+    print(acc)
+    print(np.mean(acc))
+    return acc
+
+def onerun(pzn,params_dict):
     states=[]
-    p = Puzzle(4,params_dict, path="ACT-R:tangram-solver;models;backtracking-xy-model.lisp")
+    p = Puzzle(pzn,params_dict, path="ACT-R:tangram-solver;models;backtracking-xy-model.lisp")
 
 
     p.path = f'{ROOT_DIR}/puzzle_state.png'
@@ -299,7 +413,7 @@ def onerun(params_dict):
     actr.goal_focus('start')
 
     #while p.step < 16 and p.step+p.btsteps < 30:
-    while p.step < 16 & p.step + p.btsteps < 28:
+    while p.step < 18 and p.step + p.btsteps < 31:
 
 
         if p.status == 'completed':
@@ -350,25 +464,25 @@ def seq_to_list(step_sequence):
 
 if __name__ == '__main__':
     # main()
-    results_df = pd.DataFrame(
-        columns=['run','step','small triangle', 'middle triangle', 'big triangle', 'square',
-                 'parallelogram'])
-    to_mat= []
-    for r in range(30):
-        print(f'puzzle {r}')
-        s,step_sequence = onerun({':rt': 2.4, ':mas': 6})
-        to_mat.append(seq_to_list(step_sequence))
-
-        for i in range(len(s)):
-            row = {'run' : r, 'step':(i+1),'small triangle':s[i][0],'middle triangle':s[i][1],
-                           'big triangle':s[i][2],'square':s[i][3],'parallelogram':s[i][4]}
-            results_df = results_df.append(row,ignore_index=True)
-
-    results_df.to_csv('datasets/model_states_evolution_4_large.csv')
-    length = max(map(len, to_mat))
-    mat = np.array([xi + [0] * (length - len(xi)) for xi in to_mat])
-    np.savetxt("datasets/heatmap_4_large.csv", mat, delimiter=',')
-
+    # results_df = pd.DataFrame(
+    #     columns=['run','step','small triangle', 'middle triangle', 'big triangle', 'square',
+    #              'parallelogram'])
+    # to_mat= []
+    # for r in range(30):
+    #     print(f'puzzle {r}')
+    #     s,step_sequence = onerun(2,{':rt': 2.5, ':mas': 10})
+    #     to_mat.append(seq_to_list(step_sequence))
+    #
+    #     for i in range(len(s)):
+    #         row = {'run' : r, 'step':(i+1),'small triangle':s[i][0],'middle triangle':s[i][1],
+    #                        'big triangle':s[i][2],'square':s[i][3],'parallelogram':s[i][4]}
+    #         results_df = results_df.append(row,ignore_index=True)
+    #
+    # results_df.to_csv('results/model_states_evolution_2_mixed_cnt.csv')
+    # length = max(map(len, to_mat))
+    # mat = np.array([xi + [0] * (length - len(xi)) for xi in to_mat])
+    # np.savetxt("results/heatmap_2_mixed_cnt.csv", mat, delimiter=',')
+    prediction_run(2,{':rt': 2.5, ':mas': 10},kd=1, kcv=3)
 
 '''
 ### parameters that can be changed:
@@ -385,3 +499,28 @@ parameter tuning
 results comparison function
 
 '''
+
+# def main():
+#     # results_df = pd.DataFrame(columns=['ans','rt','mas','step','small triangle', 'middle triangle','big triangle', 'square', 'parallelogram'])
+#     # grid_param = [{':rt':2,':mas':6},{':rt':2.5,':mas':6}, {':rt':2,':mas':7}, {':rt':2.5,':mas':7}]
+#     # for params_instance in grid_param:
+#     #     steps = onerun(params_instance)
+#     #     for i in range(len(steps)):
+#     #         row = {'ans':params_instance.get(':ans'), 'rt':params_instance.get(':rt'), 'mas':params_instance.get(':mas'),
+#     #                'step':(i+1)*4,'small triangle':[steps[i][3],steps[i][4]],'middle triangle':[steps[i][2]],
+#     #                'big triangle':[steps[i][0],steps[i][1]],'square':[steps[i][5]],'parallelogram':[steps[i][6]]}
+#     #         results_df = results_df.append(row,ignore_index=True)
+#     #
+#     # results_df.to_csv('param_search_results.csv')
+#     to_mat = []
+#     for i in range(31):
+#         states, step_sequence = onerun({':rt':2.3,':mas':6})
+#         to_mat.append(seq_to_list(step_sequence))
+#
+#     length = max(map(len, to_mat))
+#     mat = np.array([xi + [0] * (length - len(xi)) for xi in to_mat])
+#     np.savetxt("datasets/heatmap_4.csv",mat,delimiter=',')
+#
+# def create_state_evolution_df():
+#     for i in range(31):
+#         states, step_sequence = onerun({':rt': 2.2, ':mas': 6})
